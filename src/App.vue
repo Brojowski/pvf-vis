d9b3d1fc94deb7bc7d2ccbb7df20ca792250e46e
unknown <alex.gajowski@gmail.com>
2024-03-11 23:16:18 -0400

    init socket connection to volley station

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@ export default {
   components: {
     VuePlotly
   },
+  mounted() {
+    const plugin = document.createElement("script");
+    plugin.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.3.0/socket.io.js");
+    plugin.async = true;
+    document.head.appendChild(plugin);
+
+    setTimeout(this.startSocket, 1000)
+  },
   data() {
     return {
       max: 40,
@@ -15,8 +23,31 @@ export default {
     }
   },
   methods: {
+    startSocket() {
+      const socket = io(
+          'https://api.widgets.volleystation.com:443',
+          {
+            path: '/socket.io',
+            transports: [
+              'websocket'
+            ],
+            reconnection: !0,
+            reconnectionDelay: 1000,
+            reconnectionDelayMax: 5000,
+            reconnectionAttempts: 1 / 0,
+            query: {
+              connectionPathName: '/play-by-play/2125302',
+              token: 'PhodQuahof1ShmunWoifdedgasvuipki'
+            }
+          }
+      );
+      socket.on("widget/play-by-play updated", this.updatePlayByPlay);
+    },
+    updatePlayByPlay(data) {
+      console.log(data)
+      this.sets = data['fullScoutData']['scout']['sets']
+    },
     setScore(set) {
-
       return {
         data: [
           {
@@ -39,7 +70,6 @@ export default {
 <template>
   <div class="match">
     <VuePlotly v-for="set in sets" :data="setScore(set)" :layout="{title: 'Set #'}" :displayModeBar="true"/>
-
   </div>
 </template>
 
