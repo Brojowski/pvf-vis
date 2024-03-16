0af4d17020ac59eb1a5c21a10e65dc1c4c78d7f1
brojowski <alex.gajowski@gmail.com>
2024-03-16 13:15:27 -0400

    Style Set score charts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script>
-import SetScore from './components/SetScore.vue'
-import {data} from './socket_event_ex.js'
+import {prerecordedMatch} from './socket_event_ex.js'
 import {VuePlotly} from 'vue3-plotly'
 
 export default {
@@ -19,7 +18,8 @@ export default {
     return {
       max: 40,
       sections: [2, 5, 10, 15],
-      sets: data['widget/play-by-play updated']['fullScoutData']['scout']['sets']
+      match: prerecordedMatch,
+      sets: prerecordedMatch['fullScoutData']['scout']['sets']
     }
   },
   methods: {
@@ -45,21 +45,56 @@ export default {
     },
     updatePlayByPlay(data) {
       console.log(data)
+      this.match = data
       this.sets = data['fullScoutData']['scout']['sets']
     },
     setScore(set) {
+      let lastPt = set.events[set.events.length - 1]
+      let pointMax = Math.max(25, lastPt.score.home, lastPt.score.away) + 2
       return {
         data: [
+          {
+            x: [0, pointMax],
+            y: [0, pointMax],
+            type: 'scatter',
+            line: {color: 'rgba(229,229,229, 0.8)'},
+          },
+          { // Home win
+            x: [pointMax, 25, 25, pointMax],
+            y: [0, 0, 23, pointMax - 2],
+            type: 'scatter',
+            line: {color: this.match.teams.home.color},
+            fill: 'toself',
+          },
+          { // Away Win
+            x: [0, 0, 23, pointMax - 2],
+            y: [pointMax, 25, 25, pointMax],
+            type: 'scatter',
+            line: {color: this.match.teams.away.color},
+            fill: 'toself',
+
+          },
           {
             x: set.events.map(it => it.score.home),
             y: set.events.map(it => it.score.away),
             type: "scatter",
             line: {color: "black"},
-          }],
+          },
+        ],
         layout: {
           title: "Set Scoring",
           width: 500,
-          height: 500
+          height: 500,
+          xaxis: {
+            range: [0, pointMax],
+            dtick: 5,
+            title: this.match.teams.home.name,
+          },
+          yaxis: {
+            range: [0, pointMax],
+            dtick: 5,
+            title: this.match.teams.away.name,
+          }
         }
       }
     }
@@ -69,19 +104,23 @@ export default {
 
 <template>
   <div class="match">
-    <VuePlotly v-for="set in sets" :data="setScore(set)" :layout="{title: 'Set #'}" :displayModeBar="true"/>
+    <VuePlotly class="set-score" v-for="set in sets" :data="setScore(set)" :layout="{title: 'Set #'}" :displayModeBar="true"/>
   </div>
 </template>
 
 <style scoped>
 .match {
-  height: 500px;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-gap: 10px;
+  margin: 10px;
 }
 
 .set-score {
+  margin: auto;
 }
 
 </style>
